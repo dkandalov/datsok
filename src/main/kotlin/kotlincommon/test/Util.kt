83e8ec1250f5c0f735c6bee864c639b2317522e0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-01-01 12:21:06 +0400

    added finder of failed assertion in source code

--- src/main/kotlin/kotlincommon/test/Util.kt
+++ src/main/kotlin/kotlincommon/test/Util.kt
@@ -2,8 +2,12 @@
 
 package kotlincommon.test
 
+import kotlincommon.test.FailedAssertionFinder.withAssertionError
+import java.io.File
+import java.io.IOException
 
-infix fun <T> T.shouldEqual(that: T) {
+
+infix fun <T> T.shouldEqual(that: T) = withAssertionError("shouldEqual") {
     if (this == that) return
 
     val (expectedPostfix, actualPostfix) =
@@ -26,7 +30,58 @@ infix fun <T> T.shouldEqual(that: T) {
     )
 }
 
-infix fun <T> T.shouldNotEqual(that: T) {
+infix fun <T> T.shouldNotEqual(that: T) = withAssertionError("shouldNotEqual") {
     if (this == that) throw AssertionError("Expected value not equal to: $that")
 }
 
+
+object FailedAssertionFinder {
+    inline fun <T> withAssertionError(functionName: String, f: () -> T) {
+        try {
+            f()
+        } catch (e: AssertionError) {
+            val failedAssertion = findFailureFrame(e.stackTrace, "kotlincommon.test.UtilKt", functionName)?.readSourceCodeLine()
+            if (failedAssertion != null) System.err.println("\nFailed at: $failedAssertion")
+            throw e
+        }
+    }
+
+    fun findFailureFrame(stackTrace: Array<StackTraceElement>, className: String, methodName: String): StackTraceElement? {
+        for (i in stackTrace.indices) {
+            val element = stackTrace[i]
+            if (element.className == className && element.methodName == methodName)
+                return stackTrace[i + 1]
+        }
+        return null
+    }
+
+    fun StackTraceElement.readSourceCodeLine(): String? =
+        try {
+            findSourceCodeFor(className)
+                ?.readLines()
+                ?.get(lineNumber - 1)
+                ?.trim()
+        } catch (ignored: IOException) {
+            null
+        }
+
+    private fun findSourceCodeFor(className: String): File? {
+        val paths1 = listOf("src", "")
+        val paths2 = listOf("test", "main", "")
+        val paths3 = listOf("kotlin", "java", "")
+        val extensions = listOf(".kt", ".java")
+
+        paths1.forEach { path1 ->
+            paths2.forEach { path2 ->
+                paths3.forEach { path3 ->
+                    extensions.forEach { extension ->
+                        val path = listOf(path1, path2, path3).joinToString("/").removePrefix("/")
+                        val file = File(path + "/" + className.replace('.', '/') + extension)
+                        if (file.exists()) return file
+                    }
+                }
+            }
+        }
+        return null
+    }
+}
